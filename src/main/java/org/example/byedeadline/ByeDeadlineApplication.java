3a76c66d6cbb9a5426d0e4b1dfcd65519d6a7ff2
baloghcs96 <balogh.csaba.6991@gmail.com>
2024-07-16 10:46:33 +0200

    This is the first commit.

---
+++ src/main/java/org/example/byedeadline/ByeDeadlineApplication.java
@@ -0,0 +1,13 @@
+package org.example.byedeadline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByeDeadlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByeDeadlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/byedeadline/ByeDeadlineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.byedeadline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByeDeadlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
